thierer/sd2iec

commit 280dd2e18c9f637e75bd9961ef0fffc1b2e046f4

Clear sector error codes in D64 images when formatting

src/d64ops.c
@@ -2023,6 +2023,20 @@ uint8_t d64_set_error(uint8_t part, uint8_t track, uint8_t sector, uint8_t error
 /*  Formatting disk images                                                   */
 /* ------------------------------------------------------------------------- */
 
+/* Initializes sector contents and resets error info, if applicable. */
+static uint8_t d64_format_track(uint8_t part, buffer_t *buf, uint8_t track) {
+  uint8_t sector;
+
+  for (sector = 0; sector < sectors_per_track(part, track); sector++) {
+    if (image_write(part, sector_offset(part, track, sector), buf->data, 256, 0))
+      return 1;
+    if (partition[part].imagetype & D64_HAS_ERRORINFO)
+      d64_set_error(part, track, sector, 1); // no error
+  }
+
+  return 0;
+}
+
 /* create a 1581/DNP BAM signature */
 static void format_add_bam_signature(uint8_t doschar, uint8_t *idbuf) {
   uint8_t *ptr = bam_buffer->data + 2;
@@ -2183,11 +2197,8 @@ static void d64_format(uint8_t part, uint8_t *name, uint8_t *id) {
   if (id != NULL) {
     /* Clear the data area of the disk image */
     for (t=1; t<=get_param(part, LAST_BAM_TRACK); t++) {
-      for (s=0; s<sectors_per_track(part, t); s++) {
-        if (image_write(part, sector_offset(part, t, s),
-                        buf->data, 256, 0))
-          return;
-      }
+      if (d64_format_track(part, buf, t))
+        return;
     }
 
     /* Copy the new ID into the buffer */
@@ -2204,12 +2215,8 @@ static void d64_format(uint8_t part, uint8_t *name, uint8_t *id) {
 
     /* clear the entire directory track */
     /* This is not accurate, but I do not care. */
-    t = get_param(part, DIR_TRACK);
-    for (s=0; s < sectors_per_track(part, t); s++) {
-      if (image_write(part, sector_offset(part, t, s),
-                      buf->data, 256, 0))
-        return;
-    }
+    if (d64_format_track(part, buf, get_param(part, DIR_TRACK)))
+      return;
   }
   idbuf[2] = 0xa0;
 
@@ -2221,8 +2228,6 @@ static void d64_format(uint8_t part, uint8_t *name, uint8_t *id) {
 
   /* call imagetype-specific format function */
   partition[part].d64data.format_function(part, buf, name, idbuf);
-
-  /* FIXME: Clear the error info block */
 }
 
 

README
@@ -739,7 +739,8 @@ on non-standard tracks without error.
 
 If the image has an error info block appended it will be used to simulate
 read errors. Writing to a sector with an error will always work, but it
-will not clear the indicated error. D81 images with error info blocks are
+will not clear the indicated error. Formatting will not remove the error
+info block but clear all errors. D81 images with error info blocks are
 not supported.
 
 Warning: There is at least one program out there (DirMaster v2.1/Style by

commit 077d4f38ce6edc3bf97ab9714070e492dcd1da9e

Fix format ("N:" command) for DNP images
This was broken since 280dd2e (firmware hangs when trying to
format a DNP image).

The sector number needs to be uint16 when doing a straightforward
comparison, as DNP has 256 sectors per track.

src/d64ops.c
@@ -2075,7 +2075,7 @@ uint8_t d64_extend_image(uint8_t part, uint8_t tracks) {
 
 /* Initializes sector contents and resets error info, if applicable. */
 static uint8_t d64_format_track(uint8_t part, buffer_t *buf, uint8_t track) {
-  uint8_t sector;
+  uint16_t sector;
 
   for (sector = 0; sector < sectors_per_track(part, track); sector++) {
     if (image_write(part, sector_offset(part, track, sector), buf->data, 256, 0))